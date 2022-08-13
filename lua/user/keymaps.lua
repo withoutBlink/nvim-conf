e001db82c26907117b37345693c9ca055afdc502
withoutBlink <yeluoqiulin@gmail.com>
2022-08-14 02:37:58 +0800

    undo and redo bind to u and U, comment unused key-bindings

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -1,6 +1,6 @@
-local opts = { noremap = true, silent = true }
+local opts = { noremap = false, silent = true }
 
-local term_opts = { silent = true }
+-- local term_opts = { silent = true }
 
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
@@ -32,12 +32,17 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "L", ":bnext<CR>", opts)
+keymap("n", "H", ":bprevious<CR>", opts)
+
+
+-- Undo and Redo
+keymap("n", "u", ":undo<CR>", opts)
+keymap("n", "U", ":redo<CR>", opts)
 
 -- Move text up and down
-keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
-keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+-- keymap("n", "<A-k>", ":m .-2<CR>==gi", opts)
+-- keymap("n", "<A-j>", ":m .+1<CR>==gi", opts)
 
 -- Insert --
 -- Press jk fast to enter normal mode
@@ -46,22 +51,23 @@ keymap("i", "kj", "<ESC>", opts)
 
 keymap("v", "jk", "<ESC>", opts)
 keymap("v", "kj", "<ESC>", opts)
+
 -- Visual --
 -- Stay in indent mode
 keymap("v", "<", "<gv", opts)
 keymap("v", ">", ">gv", opts)
 
 -- Move text up and down
-keymap("v", "<A-j>", ":m .+1<CR>==", opts)
-keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "J", ":m .+1<CR>==", opts)
+keymap("v", "K", ":m .-2<CR>==", opts)
 keymap("v", "p", '"_dP', opts)
 
 -- Visual Block --
 -- Move text up and down
 keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
-keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+-- keymap("x", "<A-l>", ":move '>+1<CR>gv-gv", opts)
+-- keymap("x", "<A-h>", ":move '<-2<CR>gv-gv", opts)
 
 -- Terminal --
 -- Better terminal navigation
