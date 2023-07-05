14b41d83efaa58f424bb7a1f8c6637eb7519b187
qiulinyang <qiulinyang@fedora.local>
2023-07-05 03:58:27 -0400

    fix(help): solved can not install help document highlight problem

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -2,7 +2,7 @@ local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then return end
 configs.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "cpp", "lua", "vim", "help", "query" },
+  ensure_installed = { "c", "cpp", "lua", "vim", "query" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -12,7 +12,7 @@ configs.setup {
   auto_install = true,
 
   -- List of parsers to ignore installing (for "all")
-  ignore_install = { "javascript" },
+  ignore_install = { "javascript", "help" },
 
   ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
   -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
