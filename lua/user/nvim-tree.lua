12f23a0c0ed151ab3d32622d016eff55e272be95
qlyang <yeluoqiulin@gmail.com>
2023-07-08 23:40:00 +0800

    fix(mapping): new version of nvim-tree use on_attach config style, so change into on_attach style and add <CR> as CD command

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -9,97 +9,113 @@ if not config_status_ok then return end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
+local function on_attach(bufnr)
+	local api = require('nvim-tree.api')
+
+	local function opts(desc)
+		return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+	vim.keymap.set('n', 'l', api.node.open.edit, opts('Open'))
+	vim.keymap.set('n', '<CR>', api.tree.change_root_to_node, opts('CD'))
+	vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts('Close Directory'))
+	vim.keymap.set('n', 'v', api.node.open.vertical, opts('Open: Vertical Split'))
+end
+
 nvim_tree.setup {
-  renderer = {
-    root_folder_modifier = ":t",
-    icons = {
-      glyphs = {
-        default = "",
-        symlink = "",
-        git = {
-          unstaged = "",
-          staged = "S",
-          unmerged = "",
-          renamed = "➜",
-          deleted = "",
-          untracked = "U",
-          ignored = "◌",
-        },
-        folder = {
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-        },
-      },
-    },
-  },
-  disable_netrw = true,
-  hijack_netrw = true,
-  --[[ open_on_setup = false, ]]
-  --[[ ignore_ft_on_setup = { ]]
-  --[[   "startify", ]]
-  --[[   "dashboard", ]]
-  --[[   "alpha", ]]
-  --[[ }, ]]
-  open_on_tab = false,
-  hijack_cursor = false,
-  update_cwd = true,
-  hijack_directories = {
-    enable = true,
-    auto_open = true,
-  },
-  diagnostics = {
-    enable = true,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    },
-  },
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-    ignore_list = {},
-  },
-  system_open = {
-    cmd = nil,
-    args = {},
-  },
-  filters = {
-    dotfiles = false,
-    custom = {},
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 500,
-  },
-  actions = {
-    open_file = {
-      quit_on_open = false,
-      resize_window = true,
-    },
-  },
-  view = {
-    width = 30,
-    hide_root_folder = false,
-    side = "left",
-    mappings = {
-      custom_only = false,
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
-    },
-    number = false,
-    relativenumber = false,
-  },
-  trash = {
-    cmd = "trash",
-    require_confirm = true,
-  },
+	on_attach = on_attach,
+	-- old style key mapping settings
+	--[[ mappings = { ]]
+	--[[ 	custom_only = false, ]]
+	--[[ 	list = { ]]
+	--[[ 		{ key = { "l", "<CR>", "o" }, cb = tree_cb "edit" }, ]]
+	--[[ 		{ key = "h",                  cb = tree_cb "close_node" }, ]]
+	--[[ 		{ key = "v",                  cb = tree_cb "vsplit" }, ]]
+	--[[ 	}, ]]
+	--[[ }, ]]
+
+	--[[ open_on_setup = false, ]]
+	--[[ ignore_ft_on_setup = { ]]
+	--[[   "startify", ]]
+	--[[   "dashboard", ]]
+	--[[   "alpha", ]]
+	--[[ }, ]]
+
+	renderer = {
+		root_folder_modifier = ":t",
+		icons = {
+			glyphs = {
+				default = "",
+				symlink = "",
+				git = {
+					unstaged = "",
+					staged = "S",
+					unmerged = "",
+					renamed = "➜",
+					deleted = "",
+					untracked = "U",
+					ignored = "◌",
+				},
+				folder = {
+					default = "",
+					open = "",
+					empty = "",
+					empty_open = "",
+					symlink = "",
+				},
+			},
+		},
+	},
+	disable_netrw = true,
+	hijack_netrw = true,
+	open_on_tab = false,
+	hijack_cursor = false,
+	update_cwd = true,
+	hijack_directories = {
+		enable = true,
+		auto_open = true,
+	},
+	diagnostics = {
+		enable = true,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+		ignore_list = {},
+	},
+	system_open = {
+		cmd = nil,
+		args = {},
+	},
+	filters = {
+		dotfiles = false,
+		custom = {},
+	},
+	git = {
+		enable = true,
+		ignore = true,
+		timeout = 500,
+	},
+	actions = {
+		open_file = {
+			quit_on_open = false,
+			resize_window = true,
+		},
+	},
+	view = {
+		width = 30,
+		hide_root_folder = false,
+		side = "left",
+		number = false,
+		relativenumber = false,
+	},
+	trash = {
+		cmd = "trash",
+		require_confirm = true,
+	},
 }
