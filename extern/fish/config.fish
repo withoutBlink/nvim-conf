e64025e49ced1cf5e6fd68c3201737923d499abf
qlyang <yeluoqiulin@gmail.com>
2023-08-16 22:17:54 +0800

    Fix(extern/fish): remove fish key bindings for !! and !$

--- extern/fish/config.fish
+++ extern/fish/config.fish
@@ -20,27 +20,3 @@ set -gx ANDROID_SDK "/Users/andrew/Library/Android/sdk"
 
 # set -gx LDFLAGS "-L/opt/homebrew/opt/llvm/lib"
 # set -gx CPPFLAGS "-I/opt/homebrew/opt/llvm/include"
-
-function bind_bang
-    switch (commandline -t)[-1]
-        case "!"
-            commandline -t -- $history[1]
-            commandline -f repaint
-        case "*"
-            commandline -i !
-    end
-end
-
-function bind_dollar
-    switch (commandline -t)[-1]
-        case "!"
-            commandline -f backward-delete-char history-token-search-backward
-        case "*"
-            commandline -i '$'
-    end
-end
-
-function fish_user_key_bindings
-    bind ! bind_bang
-    bind '$' bind_dollar
-end
