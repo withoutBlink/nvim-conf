d6f1dce8505d86f8fd3a2cef18509751d65edf64
qlyang <yeluoqiulin@gmail.com>
2023-09-06 01:11:52 +0800

    Feat(extern/fish): add fisher install and z install function, remove macos unique settings

--- extern/fish/config.fish
+++ extern/fish/config.fish
@@ -3,20 +3,20 @@ if status is-interactive
 end
 
 fish_vi_key_bindings
-set -g JAVA_HOME '/opt/homebrew/Cellar/openjdk@17/17.0.7/libexec/openjdk.jdk/Contents/Home'
 
 alias ls "ls -F"
 alias ll "exa --long --header --git -F --icons"
-alias strace "dtruss"
-alias ldd "otool -L"
-alias ipa "ifconfig en0"
 
 set fish_color_normal brcyan
 set fish_color_command brcyan
 
 set -gx LS_COLORS "di=32" 
-set -gx HOMEBREW_NO_AUTO_UPDATE 1
-set -gx ANDROID_SDK "/Users/andrew/Library/Android/sdk"
 
-# set -gx LDFLAGS "-L/opt/homebrew/opt/llvm/lib"
-# set -gx CPPFLAGS "-I/opt/homebrew/opt/llvm/include"
+function install_fisher
+	curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+end
+
+function install_z
+	fisher install jethrokuan/z
+end
+
