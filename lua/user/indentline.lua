747d583be034aca578624e0cc7806415419eefad
withoutBlink <yeluoqiulin@gmail.com>
2023-11-08 12:54:48 +0800

    fix(indentline): update to version 3 config

--- lua/user/indentline.lua
+++ lua/user/indentline.lua
@@ -1,4 +1,4 @@
-local status_ok, indent_blankline = pcall(require, "indent_blankline")
+local status_ok, indent_blankline = pcall(require, "ibl")
 if not status_ok then return end
 
 vim.g.indent_blankline_buftype_exclude = { "terminal", "nofile" }
@@ -58,7 +58,7 @@ vim.wo.colorcolumn = "99999"
 indent_blankline.setup({
 	-- show_end_of_line = true,
 	-- space_char_blankline = " ",
-	show_current_context = true,
+	-- show_current_context = true,
 	-- show_current_context_start = true,
 	-- char_highlight_list = {
 	--   "IndentBlanklineIndent1",
