355cc19d1200e67e037637fff745618061daeb54
withoutBlink <yeluoqiulin@gmail.com>
2023-11-08 12:59:45 +0800

    fix(nvim-tree): new style configuration without nvim-tree.config

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -4,10 +4,10 @@
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then return end
 
-local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
-if not config_status_ok then return end
+-- local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
+-- if not config_status_ok then return end
 
-local tree_cb = nvim_tree_config.nvim_tree_callback
+-- local tree_cb = nvim_tree_config.nvim_tree_callback
 
 local function on_attach(bufnr)
 	local api = require('nvim-tree.api')
@@ -109,7 +109,6 @@ nvim_tree.setup {
 	},
 	view = {
 		width = 30,
-		hide_root_folder = false,
 		side = "left",
 		number = false,
 		relativenumber = false,
