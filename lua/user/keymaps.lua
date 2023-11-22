42fdea153060b0c91dc4325177b7d5a43185eb4d
qlyang <yeluoqiulin@gmail.com>
2023-11-22 23:56:52 +0800

    Feat(keymaps): forbid 'c' to change any document

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -81,3 +81,7 @@ keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 -- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 -- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 -- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+
+-- Unbindings --
+keymap("n", "c", "<ESC>", opts)
+keymap("v", "c", "<ESC>", opts)
