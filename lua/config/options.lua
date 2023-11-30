9c62b9f37421fdafd6486c083fda4a088690704b
qlyang <yeluoqiulin@gmail.com>
2023-11-30 14:53:47 +0800

    Feat(config/options): add basic vim options of autoforamt, tabs and mouse

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Trun off autoformat
+vim.g.autoformat = false
+
+-- Set vim to use tabs
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = false
+
+--Set vim to ignore mouse event
+vim.opt.mouse = ""
