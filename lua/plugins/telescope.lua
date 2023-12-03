d92152f1ddb52d3c04993aa4d1ec78a16a6b3b49
qlyang <yeluoqiulin@gmail.com>
2023-12-03 15:25:22 +0800

    Fix(plugins/telescope): TODO: limit search scope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,17 +1,56 @@
+local util = require("lazyvim.util")
+local builtin = require("telescope.builtin")
+
+-- get current project folder during loading
+local project_pwd = util.root()
+
 local telescope_keymaps = {
-  -- add a keymap to browse plugin files
-  -- stylua: ignore
-  {
-    "<leader>f",
-    "<cmd>Telescope find_files<CR>",
-    desc = "Find Files",
-    nowait = true,
-  },
-	-- TODO: limit search scope
+	{
+		"<leader>f",
+		function ()
+			builtin.find_files({ cwd = project_pwd })
+		end,
+		desc = "Find Files",
+		nowait = true,
+	},
 	{
 		"<leader>F",
-		"<cmd>Telescope live_grep<CR>",
-		desc = "Find Symbols",
+		function ()
+			builtin.live_grep({ cwd = project_pwd })
+		end,
+		desc = "Find Symbol",
+		nowait = true,
+	},
+	{
+		"<leader>S",
+		function ()
+			builtin.grep_string({ cwd = project_pwd })
+		end,
+		desc = "Symbol Under Cursor",
+		nowait = true,
+	},
+	{
+		"<leader>ss",
+		function()
+			require("telescope.builtin").grep_string({ cwd = vim.fn.expand("%:p:h") })
+		end,
+		desc = "Under Cursor(Current)",
+		nowait = true,
+	},
+	{
+		"<leader>sf",
+		function()
+			require("telescope.builtin").find_files({ cwd = vim.fn.expand("%:p:h") })
+		end,
+		desc = "Files(Current)",
+		nowait = true,
+	},
+	{
+		"<leader>sF",
+		function()
+			require("telescope.builtin").live_grep({ cwd = vim.fn.expand("%:p:h") })
+		end,
+		desc = "Symbols(Currnet)",
 		nowait = true,
 	},
 	{
@@ -19,7 +58,7 @@ local telescope_keymaps = {
 		function()
 			require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
 		end,
-		desc = "Find Plugin Files",
+		desc = "Find",
 	},
 }
 
