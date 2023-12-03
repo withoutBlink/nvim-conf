364d917ba52c5e61f6c8596768ec13ca8412beb7
qlyang <yeluoqiulin@gmail.com>
2023-12-03 21:09:52 +0800

    Fix(plugins/lsp): Simplified keymap setting for lsp

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,9 +1,69 @@
-local lsp_handle = require("plugins.lsp.handle")
+local lspkeys = {
+	{
+		"gd",
+		function()
+			require("telescope.builtin").lsp_definitions({ reuse_win = true })
+		end,
+		desc = "Goto Definition",
+	},
+	{
+		"gr",
+		"<cmd>Telescope lsp_references<cr>",
+		desc = "References",
+	},
+	{
+		"gD",
+		vim.lsp.buf.declaration,
+		desc = "Goto Declaration",
+	},
+	{
+		"gI",
+		function()
+			require("telescope.builtin").lsp_implementations({ reuse_win = true })
+		end,
+		desc = "Goto Implementation",
+	},
+	{
+		"gy",
+		function()
+			require("telescope.builtin").lsp_type_definitions({ reuse_win = true })
+		end,
+		desc = "Goto T[y]pe Definition"
+	},
+	{
+		"gK",
+		vim.lsp.buf.signature_help,
+		desc = "Signature Help",
+	},
+	{
+		"gh",
+		"<cmd>ClangdSwitchSourceHeader<cr>",
+		desc = "Switch Header/Source",
+	},
+	{
+		"K",
+		vim.lsp.buf.hover,
+		desc = "Hover",
+	},
+	{
+		"<c-k>",
+		vim.lsp.buf.signature_help,
+		mode = "i",
+		desc = "Signature Help",
+	},
+	{
+		"<leader>r",
+		vim.lsp.buf.rename,
+		desc = "Rename",
+	},
+}
 
 local M = {
 	{
 		"neovim/nvim-lspconfig",
-		config = lsp_handle.config,
+		keys = function()
+			return lspkeys
+		end,
 	},
 	{
 		"williamboman/mason.nvim",
