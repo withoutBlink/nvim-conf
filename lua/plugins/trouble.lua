88b5a120ee49e75e1132910d171584dec8f1b185
qlyang <yeluoqiulin@gmail.com>
2023-12-08 08:51:48 +0800

    Style(plugins/trouble): add depth of return block

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -1,22 +1,24 @@
 local M = {
-	"folke/trouble.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	keys = {
-		{
-			"<leader>d",
-			function()
-				require("trouble").toggle("document_diagnostics")
-			end,
-			desc = "Doc Diag",
-			nowait = true,
-		},
-		{
-			"<leader>D",
-			function()
-				require("trouble").toggle("workspace_diagnostics")
-			end,
-			desc = "Space Diag",
-			nowait = true,
+	{
+		"folke/trouble.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		keys = {
+			{
+				"<leader>d",
+				function()
+					require("trouble").toggle("document_diagnostics")
+				end,
+				desc = "Document Diag",
+				nowait = true,
+			},
+			{
+				"<leader>D",
+				function()
+					require("trouble").toggle("workspace_diagnostics")
+				end,
+				desc = "Project Diag",
+				nowait = true,
+			},
 		},
 	},
 }
