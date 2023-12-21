f7a247d069f734e80e953f8f2d0fe0b43389de51
qlyang <yeluoqiulin@gmail.com>
2023-12-21 13:51:47 +0800

    Fix(config/options): remove ignorecase from options, which couses search error

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,11 +6,19 @@
 vim.g.autoformat = false
 
 -- Set vim to use tabs
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = false
-vim.opt.ignorecase = true
+
+--[[
+Use \c to ignore case and \C to match case,
+other than use vim opt ignorecase,
+because it will ignore case when searching for a capital letter
+]]
 vim.opt.smartcase = true
+vim.opt.ignorecase = false
 
 --Set vim to ignore mouse event
 vim.opt.mouse = ""
+
+-- TODO: Set vim to notice _ and uppercase as word
