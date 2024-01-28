e929447f1bf4cdeb05b6ee9aedba56be4ea0ea73
qlyang <yeluoqiulin@gmail.com>
2024-01-28 11:22:54 +0800

    Fix(plugins/bufferline): remove unnecessary options in disabled plugin

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,17 +3,6 @@ local M = {
 	{
 		"akinsho/bufferline.nvim",
 		enabled = false,
-		keys = function()
-			return {}
-		end,
-		opts = {
-			options = {
-				close_icon = '',
-				buffer_close_icon = '',
-				show_buffer_close_icons = false,
-				show_close_icon = false,
-			}
-		}
 	}
 }
 
