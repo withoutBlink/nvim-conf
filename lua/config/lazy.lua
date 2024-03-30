9146dcefb057e2ccf2b5609bc3839bff12804103
qlyang <yeluoqiulin@gmail.com>
2024-03-30 11:34:38 +0800

    Fix(config/lazy): remove clangd extra for remove mason and use native lspconfig

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -23,7 +23,7 @@ require("lazy").setup({
 			},
 		},
 		-- import any extras modules here
-		{ import = "lazyvim.plugins.extras.lang.clangd" },
+		-- { import = "lazyvim.plugins.extras.lang.clangd" },
 		{ import = "lazyvim.plugins.extras.coding.copilot" },
 		-- { import = "lazyvim.plugins.extras.lang.typescript" },
 		-- { import = "lazyvim.plugins.extras.lang.json" },
