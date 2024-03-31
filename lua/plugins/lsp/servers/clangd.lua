8c7f1daa8d91f28c29938d072e5577744247f9e9
qlyang <yeluoqiulin@gmail.com>
2024-03-31 23:48:23 +0800

    Fix(plugins/lsp): clangd can not find right project root dir. when compile_commands.json not exist and go to file will cause the project root dir changing. So there should be a static project root dir determined once open a folder or file. Prevent clangd for parsing external project as a new project

--- lua/plugins/lsp/servers/clangd.lua
+++ lua/plugins/lsp/servers/clangd.lua
@@ -6,6 +6,10 @@ local M = {
 	mason = false;
 	-- Use no default keys when load changd server
 	keys = false;
+	-- TODO: Emergently fix for clangd to find the project root when there is no compile_commands.json
+	root_dir = function(fname)
+		return lspconfig.util.root_pattern("compile_commands.json", ".git")(fname) or lspconfig.util.path.dirname(fname)
+	end;
 }
 
 return M
