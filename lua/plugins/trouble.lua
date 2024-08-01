61a87d2a9d71c16169f98aaf92f4e06e1704ff8f
yangqiulin <qiulin.yang@ibe.cn>
2024-08-01 16:36:49 +0800

    Fix(plugins/trouble): use function to override default key bindings

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -2,25 +2,27 @@ local M = {
 	{
 		"folke/trouble.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
-		keys = {
-			{
-				"<leader>d",
-				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-				desc = "Document Diag",
-				nowait = true,
-			},
-			{
-				"<leader>D",
-				"<cmd>Trouble diagnostics toggle<cr>",
-				desc = "Project Diag",
-				nowait = true,
-			},
-			{
-				"<leader>t",
-				"<cmd>Trouble symbols toggle focus=true win.size=45<cr>",
-				desc = "Symbols (Trouble)",
-			},
-		},
+		keys = function()
+			return {
+				{
+					"<leader>d",
+					"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+					desc = "Document Diag",
+					nowait = true,
+				},
+				{
+					"<leader>D",
+					"<cmd>Trouble diagnostics toggle<cr>",
+					desc = "Project Diag",
+					nowait = true,
+				},
+				{
+					"<leader>t",
+					"<cmd>Trouble symbols toggle focus=true win.size=45<cr>",
+					desc = "Symbols (Trouble)",
+				},
+			}
+		end,
 	},
 }
 
