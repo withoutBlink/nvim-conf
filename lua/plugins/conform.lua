579825794e19c7b9b72bd1a0ed66e9da14f608cb
qlyang <yeluoqiulin@gmail.com>
2024-11-18 09:48:28 +0800

    Fix(plugins/conform): using correct way to delete defaut keys

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,12 +1,8 @@
-local conform_keymaps = {}
-
 local M = {
 	{
 		"stevearc/conform.nvim",
 		event = { "BufWinEnter" },
-		keys = function()
-			return conform_keymaps
-		end,
+		keys = false,
 	},
 }
 
