55a1c2ef96196385b3feb4293061b93b433d6cb3
qiulinyang <abcdef@mail.com>
2025-02-24 10:36:17 +0800

    Fix(plugins/dap): merge errors

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -42,57 +42,6 @@ local M = {
 		end,
 		keys = {
 			{
-<<<<<<< HEAD
-				mode = { 'n' },
-				'<leader>br',
-				'<cmd>DapContinue<cr>',
-				desc = 'Start(Continue) Debugging',
-			},
-			{
-				mode = { 'n' },
-				'<leader>bb',
-				'<cmd>DapToggleBreakpoint<cr>',
-				desc = 'Toggle Breakpoint',
-			},
-			{
-				mode = { 'n' },
-				'<leader>bn',
-				'<cmd>DapStepOver<cr>',
-				desc = 'Step Over',
-			},
-			{
-				mode = { 'n' },
-				'<leader>bs',
-				'<cmd>DapStepInto<cr>',
-				desc = 'Step Into',
-			},
-			{
-				mode = { 'n' },
-				'<leader>bo',
-				'<cmd>DapStepOut<cr>',
-				desc = 'Step Out',
-			},
-			{
-				mode = { 'n' },
-				'<leader>be',
-				'<cmd>DapToggleRepl<cr>',
-				desc = 'Toggle DBG Command Line',
-			},
-			{
-				mode = { 'n' },
-				'<leader>bd',
-				'<cmd>DapDisconnect<cr>',
-				desc = 'Disconnect',
-			},
-			{
-				mode = { 'n' },
-				'<leader>bt',
-				'<cmd>DapTerminate<cr>',
-				desc = 'Terminate',
-			}
-		},
-	}
-=======
 				mode = { "n" },
 				"<leader>br",
 				"<cmd>DapContinue<cr>",
@@ -142,7 +91,6 @@ local M = {
 			},
 		},
 	},
->>>>>>> origin/macos
 }
 
 return M
