ab6225a22c63c72603353e8a0bee08882c6186c1
andrew <yeluoqiulin@gmail.com>
2025-03-15 23:13:01 +0800

    Fix(plugins/blink): use blink super-tab

---
+++ lua/plugins/blink.lua
@@ -0,0 +1,21 @@
+local M = {
+	"saghen/blink.cmp",
+	opts = {
+		keymap = {
+      preset = 'none',
+			["<Tab>"] = {
+				function(cmp)
+					if cmp.snippet_active() then
+						return cmp.accept()
+					else
+						return cmp.select_and_accept()
+					end
+				end,
+				"snippet_forward",
+				"fallback",
+			},
+		},
+	},
+}
+
+return M
