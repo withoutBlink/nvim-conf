0d75b3e9f3356f8414c9049b185239e31891e1f9
qlyang <yeluoqiulin@gmail.com>
2025-07-24 23:30:02 +0800

    Feat(plugins/indentline): add indentlien draw

---
+++ lua/plugins/indentline.lua
@@ -0,0 +1,40 @@
+local M = {
+  {
+    "echasnovski/mini.indentscope",
+    version = false,
+    opts = {
+      -- symbol = "▏",
+      symbol = "│",
+      options = { try_as_border = true },
+      draw = {
+        delay = 10,
+      }
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = {
+          "Trouble",
+          "alpha",
+          "dashboard",
+          "fzf",
+          "help",
+          "lazy",
+          "mason",
+          "neo-tree",
+          "notify",
+          "snacks_dashboard",
+          "snacks_notif",
+          "snacks_terminal",
+          "snacks_win",
+          "toggleterm",
+          "trouble",
+        },
+        callback = function()
+          vim.b.miniindentscope_disable = true
+        end,
+      })
+    end,
+  }
+}
+
+return M
