ba9dd60c80b9d109f7311b972075f0f1e7ea0a26
qlyang <yeluoqiulin@gmail.com>
2025-09-15 14:50:37 +0800

    Feat(plugins/lsp): add lsp client for swift

--- lua/plugins/lsp/servers/init.lua
+++ lua/plugins/lsp/servers/init.lua
@@ -1,9 +1,11 @@
 local clangd_config = require("plugins.lsp.servers.clangd")
+local swift_config = require("plugins.lsp.servers.swift")
 local gopls_config = require("plugins.lsp.servers.gopls")
 
 local M = {
   clangd = clangd_config,
   gopls = gopls_config,
+  sourcekit = swift_config,
   lua_ls = {},
   ts_ls = {},
   rust_analyzer = {},
---
+++ lua/plugins/lsp/servers/swift.lua
@@ -0,0 +1,12 @@
+
+local M = {
+  capabilities = {
+    workspace = {
+      didChangeWatchedFiles = {
+        dynamicRegistration = true,
+      },
+    },
+  },
+}
+
+return M
