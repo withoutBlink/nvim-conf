bbca8dbfc4b6c6b0d2be1d87b0f13a0566a83fdf
qlyang <yeluoqiulin@gmail.com>
2025-10-08 10:08:22 +0800

    Fix(plugins/lsp): add ts/js lsp independent config

--- lua/plugins/lsp/servers/init.lua
+++ lua/plugins/lsp/servers/init.lua
@@ -1,13 +1,15 @@
 local clangd_config = require("plugins.lsp.servers.clangd")
 local swift_config = require("plugins.lsp.servers.swift")
 local gopls_config = require("plugins.lsp.servers.gopls")
+local tsls_config = require("plugins.lsp.servers.tsls")
+
 
 local M = {
   clangd = clangd_config,
   gopls = gopls_config,
   sourcekit = swift_config,
+  ts_ls = tsls_config,
   lua_ls = {},
-  ts_ls = {},
   rust_analyzer = {},
 }
 
