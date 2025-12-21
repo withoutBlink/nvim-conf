33c2d1d6e119714c145f41140fdfce2c76321929
qlyang <yeluoqiulin@gmail.com>
2025-12-22 00:00:17 +0800

    Fix(plugins/lsp): remove uncomplete swfit(sourcekit) lsp

--- lua/plugins/lsp/servers/init.lua
+++ lua/plugins/lsp/servers/init.lua
@@ -1,5 +1,4 @@
 local clangd_config = require("plugins.lsp.servers.clangd")
-local swift_config = require("plugins.lsp.servers.swift")
 local gopls_config = require("plugins.lsp.servers.gopls")
 local tsls_config = require("plugins.lsp.servers.tsls")
 local als_config = require("plugins.lsp.servers.als");
@@ -8,7 +7,6 @@ local als_config = require("plugins.lsp.servers.als");
 local M = {
   clangd = clangd_config,
   gopls = gopls_config,
-  sourcekit = swift_config,
   arduino_language_server = als_config,
   ts_ls = tsls_config,
   lua_ls = {},
