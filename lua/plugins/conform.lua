67b3e40ff8a175b02621462cc605b26307b5c8c9
withoutBlink <yqiulin@sina.com>
2025-12-31 17:29:38 +0800

    Fix(plugins/conform): use rustfmt to support rust code format selection

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,6 +3,11 @@ local M = {
     "stevearc/conform.nvim",
     event = { "BufWinEnter" },
     keys = false,
+    opts = {
+      formatters_by_ft = {
+        rust = {"rustfmt"}
+      }
+    }
   },
 }
 
