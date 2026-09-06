903808c6a31cbae867bcd5651dc440bccb824293
qlyang <yeluoqiulin@gmail.com>
2026-09-07 00:01:55 +0800

    Fix(plugins/pair): remove auto pair from command input

--- lua/plugins/pair.lua
+++ lua/plugins/pair.lua
@@ -3,7 +3,7 @@ local M = {
     "echasnovski/mini.pairs",
     event = "VeryLazy",
     opts = {
-      modes = { insert = true, command = true, terminal = false },
+      modes = { insert = true, command = false, terminal = false },
       -- skip autopair when next character is one of these
       skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
       -- skip autopair when the cursor is inside these treesitter nodes
